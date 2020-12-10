d197f83b3059c4469f5ad112e1ebecfabadcd4aa
jellymlg <39315532+jellymlg@users.noreply.github.com>
2020-12-10 13:24:44 +0100

    remove log

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ new MutationObserver(mutations => {
         if(mutation.target.getAttribute("class") == "kif_t") {
             let x = document.getElementsByClassName("kif_t");
             for(let i = 0; i < x.length; i++) {
-                console.log(x.item(i));
                 x.item(i).outerHTML = x.item(i).outerHTML;
                 x.item(i).style = "border: 3px solid green; background-color: rgba(0, 128, 0, 0.15);";
             }
